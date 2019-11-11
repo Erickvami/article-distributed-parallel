a171a6794487a954ee272fb559cbfdae7098b937
JJ Merelo <jjmerelo@gmail.com>
2019-11-11 19:57:54 +0100

    First charts

--- data/charts.R
+++ data/charts.R
@@ -3,5 +3,10 @@ library(ggthemes)
 
 data <- read.csv("experiments.csv")
 
+data$Dimensions <- as.factor(data$Dimensions)
 data.Rastrigin <- data[ data$Function == "Rastrigin", ]
+
+ggplot(data.Rastrigin, aes(x=Dimensions,y=Fitness,color=Algorithm))+geom_point()
+ggplot(data.Rastrigin, aes(x=Dimensions,y=Fitness,fill=Algorithm))+geom_boxplot()
+
 
--- data/process-data.p6
+++ data/process-data.p6
@@ -6,7 +6,6 @@ my @data = "experimentsResults.csv".IO.slurp.lines;
 
 my @dims = <2 3 5 10 20 40>;
 my %dimensions = (0..5).map: { $_ + 1 => @dims[$_] };
-say %dimensions;
 for @data[1..*] -> $l {
     my @columns = $l.split(",");
     my ($function,$algorithm) = @columns[0].split(" ");
