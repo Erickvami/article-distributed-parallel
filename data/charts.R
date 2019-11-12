1f33c0e759c2dbf7b8150e853005e05a8451a857
JJ Merelo <jjmerelo@gmail.com>
2019-11-12 07:38:12 +0100

    Adds figures and plotting script

--- data/charts.R
+++ data/charts.R
@@ -7,6 +7,11 @@ data$Dimensions <- as.factor(data$Dimensions)
 data.Rastrigin <- data[ data$Function == "Rastrigin", ]
 
 ggplot(data.Rastrigin, aes(x=Dimensions,y=Fitness,color=Algorithm))+geom_point()
-ggplot(data.Rastrigin, aes(x=Dimensions,y=Fitness,fill=Algorithm))+geom_boxplot()
+ggplot(data.Rastrigin, aes(x=Dimensions,y=Fitness,fill=Algorithm))+geom_boxplot(notch=TRUE)+theme_tufte()
+ggsave("rastrigin-boxplot.png",width=190, units="mm")
 
+data.Sphere <- data[ data$Function == "Sphere", ]
 
+ggplot(data.Sphere, aes(x=Dimensions,y=Fitness,color=Algorithm))+geom_point()+ylim(0,0.05)+theme_tufte()
+ggplot(data.Sphere, aes(x=Dimensions,y=Fitness,fill=Algorithm))+geom_boxplot(notch=TRUE)+ylim(0,0.05)+theme_tufte()
+ggsave("sphere-boxplot.png",width=190, units="mm")
