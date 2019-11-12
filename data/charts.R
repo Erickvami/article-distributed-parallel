e9b92082151e9dfaea541a1e66ac6f19bcdeb011
JJ Merelo <jjmerelo@gmail.com>
2019-11-12 07:43:11 +0100

    Adds new chart, not too clear

--- data/charts.R
+++ data/charts.R
@@ -13,5 +13,11 @@ ggsave("rastrigin-boxplot.png",width=190, units="mm")
 data.Sphere <- data[ data$Function == "Sphere", ]
 
 ggplot(data.Sphere, aes(x=Dimensions,y=Fitness,color=Algorithm))+geom_point()+ylim(0,0.05)+theme_tufte()
-ggplot(data.Sphere, aes(x=Dimensions,y=Fitness,fill=Algorithm))+geom_boxplot(notch=TRUE)+ylim(0,0.05)+theme_tufte()
+ggplot(data.Sphere, aes(x=Dimensions,y=Fitness,fill=Algorithm))+geom_boxplot(notch=TRUE)+ylim(0,0.05)+scale_y_log10()+theme_tufte()
 ggsave("sphere-boxplot.png",width=190, units="mm")
+
+data.Rosenbrock <- data[ data$Function == "Rosenbrock", ]
+
+ggplot(data.Rosenbrock, aes(x=Dimensions,y=Fitness,color=Algorithm))+geom_point()+theme_tufte()
+ggplot(data.Rosenbrock, aes(x=Dimensions,y=Fitness,fill=Algorithm))+geom_boxplot(notch=TRUE)+scale_y_log10()+ theme_tufte()
+ggsave("rosenbrock-boxplot.png",width=190, units="mm")
